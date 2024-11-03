8ddbac6d2f8e2ca499d3ab737aa82dbc0044bed1
yak <yak@isaacyakl.com>
2024-11-03 01:55:29 -0800

    Make embed responsive by default unless w and h are provided

--- src/wordpress-plugin/yt-perma-live-stream-embed/yt-perma-live-stream-embed.php
+++ src/wordpress-plugin/yt-perma-live-stream-embed/yt-perma-live-stream-embed.php
@@ -14,9 +14,8 @@ function yt_perma_live_stream_embed_shortcode($atts) {
     $channelId = $options['channel_id'] ?? ''; // Lofi Girl test channel ID: UCSJ4gkVC6NrvII8umztf0Ow
     
     // Set default shortcode attributes
-    $default_width = '1280';
-    $default_height = '720';
-    $default_responsive = true;
+    $default_width = '';
+    $default_height = '';
 
     // Extract shortcode attributes
     $attributes = shortcode_atts(
@@ -70,14 +69,22 @@ function yt_perma_live_stream_embed_shortcode($atts) {
 
     $videoId = $data['items'][0]['id']['videoId'];
 
-     // Determine iframe style based on responsive setting
-    $style = $attributes['responsive'] ? 'left: 0; width: 100%; height: 0; position: relative; padding-bottom: 56.25%;' : '';
+    if(!empty($attributes['width']) && !empty($attributes['height'])) {
+        $style = '';
+        $width =  esc_attr($attributes['width']);
+        $height =  esc_attr($attributes['height']);
+    } 
+    else {
+        $style = 'aspect-ratio: 16 / 9; width: 100%;'; // Responsive by default
+        $width = '';
+        $height = '';
+    }
 
     $embedHtml = sprintf(
-        '<div style="%s"><iframe width="%s" height="%s" src="https://www.youtube.com/embed/%s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe></div>',
-        esc_attr($style),
-        esc_attr($attributes['width']),
-        esc_attr($attributes['height']),
+        '<iframe style="%s" width="%s" height="%s" src="https://www.youtube.com/embed/%s" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>',
+        $style,
+        $width,
+        $height,
         esc_attr($videoId)
     );
 
